4154d44988f95aad3fdd95021c3cd1aff190c3f5
smoothie <79041639+mevshpret@users.noreply.github.com>
2025-06-26 22:29:45 +0300

    b

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,6 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    optimizeDeps: {
-      include: ['axios', 'izitoast'],
-    },
-
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
